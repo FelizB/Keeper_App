f2ffe0fab01478a13935039fa7401d6ff18fd994
Felix <bosirefelix97@gmail.com>
2023-05-28 23:40:44 +0300

    react keeper App

---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Note(){
+
+    return(
+        <div className='note'>
+            <h1>Note number 1</h1>
+              <p>This is my first note to make as a project that will stand on its own</p>
+            <h1>Note number 2</h1>
+              <p>This is my first note to make as a project that will stand on its own</p>
+            <h1>Note number 3</h1>
+              <p>This is my first note to make as a project that will stand on its own</p>
+            <h1>Note number 4</h1>
+              <p>This is my first note to make as a project that will stand on its own</p>
+        </div>
+    )
+}
+export default Note;
